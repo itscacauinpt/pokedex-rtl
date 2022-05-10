ddd3f2f489bd0be658e25637110b9af074f54b0c
cacau <anaclaudia.sza@outlook.com>
2022-05-10 16:44:19 -0300

    Req III - FavPoke - 100

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testing the Favortie Pokemon component', () => {
+  it('tests if the right message apprears when no favorite poke added', () => {
+    render(<FavoritePokemons />);
+
+    const noFavoriteText = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavoriteText).toBeInTheDocument();
+  });
+
+  it('tests if the pokemons marked as favorite appeared on the page', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const pokeDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(pokeDetails);
+    const addFavPoke = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(addFavPoke);
+
+    history.goForward('/favorites');
+    const favMarked = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favMarked).toBeInTheDocument();
+    history.goBack('/home');
+  });
+});
