93ec3c9992fb895a2e2a6fdc4b669a0abbf5b044
cacau <anaclaudia.sza@outlook.com>
2022-05-09 23:44:10 -0300

    Req I - App - 100 cases striked

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testeing the App component', () => {
+  it('tests if the links text and ref are correct', () => {
+    // reminder: browserRouter is no more inside the app, now is in the index,sooo we'll put the memoryRouter in this app that we render freshy freshy
+    render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+    );
+
+    const navLinks = screen.getAllByRole('link');
+    expect(navLinks[0]).toHaveTextContent(/home/i);
+    expect(navLinks[1]).toHaveTextContent(/about/i);
+    expect(navLinks[2]).toHaveTextContent(/favorite pokémons/i);
+
+  });
+
+  it('tests if the links are redirected to the right paths when clicked', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const navLinkHome = screen.getByRole('link', { name: /home/i });
+    userEvent.click(navLinkHome);
+    history.canGo('/home')
+    const headingHome = screen.getByRole('heading', {  name: /encountered pokémons/i})
+    expect(headingHome).toBeInTheDocument();
+
+    const navLinkAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(navLinkAbout);
+    history.canGo('/about')
+    const headingAbout = screen.getByRole('heading', {  name: /about pokédex/i});
+    expect(headingAbout).toBeInTheDocument();
+
+    const navLinkFavPoke = screen.getByRole('link', { name: /favorite pokémons/i});
+    userEvent.click(navLinkFavPoke);
+    history.canGo('/favorite')
+    const headingFavPoke = screen.getByRole('heading', {  name: /favorite pokémons/i});
+    expect(headingFavPoke).toBeInTheDocument();
+  })
+
+  it('tests the 404 page, it has to appear when redirected to a non existing route', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/sumiu');
+    const headingNotFound = screen.getByRole('heading', {  name: /page requested not found/i});
+    expect(headingNotFound).toBeDefined();
+  })
+});
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+// render with Router in any component
+// make sure to returns the render response as the history
+// so we can use on the tests! :>
+// using history we can navigate through the site
+// when we use history, in this case, we have to use Router 
+
+function renderWithRouter(component) {
+  const customHist = createMemoryHistory();
+  // console.log(customHist); // oh negocio bunito senhor T-T
+
+  const returnedRender = render(
+    <Router history={ customHist }>
+      {component}
+    </Router>
+  );
+
+  return {
+    history: customHist,
+    ...returnedRender, // retornando tudo, fundindo com o que eu já tenho no history
+  }
+}
+
+export default renderWithRouter
