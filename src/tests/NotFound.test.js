c2cd2bcfb2477a3689894805fd2387fc63063a60
cacau <anaclaudia.sza@outlook.com>
2022-05-10 17:30:21 -0300

    Req IV - NotFound - 100

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Test the NotFound component', () => {
+  it('tests if the page contains a heading woth right text and level', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/sumiu');
+    const headingNotFound = screen
+      .getByRole('heading', { name: /page requested not found/i });
+    expect(headingNotFound).toBeDefined();
+  });
+  it('tests if the page contains the rigth image', () => {
+    render(<NotFound />);
+    const imagePikachu = screen
+      .getByAltText(/pikachu crying because the page requested was not found/i);
+    expect(imagePikachu).toBeInTheDocument();
+  });
+});
